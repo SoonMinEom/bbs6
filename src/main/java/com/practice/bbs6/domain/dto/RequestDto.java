6d2fcbf937ce1de9ecf9127e48084d30ec5bfb79
soonmineom <skdlfma123@gmail.com>
2022-11-17 15:13:12 +0900

    Post add() Test 추가

--- src/main/java/com/practice/bbs6/domain/dto/RequestDto.java
+++ src/main/java/com/practice/bbs6/domain/dto/RequestDto.java
@@ -1,10 +1,12 @@
 package com.practice.bbs6.domain.dto;
 
 import com.practice.bbs6.domain.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
+@AllArgsConstructor
 public class RequestDto {
     private String username;
     private String password;
--- src/test/java/com/practice/bbs6/controller/UserControllerTest.java
+++ src/test/java/com/practice/bbs6/controller/UserControllerTest.java
@@ -1,5 +1,8 @@
 package com.practice.bbs6.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.bbs6.domain.dto.RequestDto;
 import com.practice.bbs6.domain.dto.ResponseDto;
 import com.practice.bbs6.service.UserService;
 import org.junit.jupiter.api.DisplayName;
@@ -7,9 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,17 +20,20 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(UserRestController.class)
 class UserControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     UserService userService;
 
     @Test
-    @DisplayName("유저 정보가 잘 받아와 지는지")
+    @DisplayName("유저 정보가 잘 조회 되는지")
     void getUser() throws Exception {
         ResponseDto responseDto = new ResponseDto("soonmin","1234");
         given(userService.get(1L)).willReturn(responseDto);
@@ -43,6 +49,31 @@ class UserControllerTest {
                 .andDo(print());
 
         verify(userService).get(1L);
+    }
+
+    @Test
+    @DisplayName("유저 정보가 잘 등록 되는지")
+    void addUser() throws Exception {
+        RequestDto requestDto = new RequestDto("soonmin","1234");
+        ResponseDto responseDto = new ResponseDto("soonmin","1234");
+        responseDto.setNameCheckMessage("등록 완료");
+
+        given(userService.add(requestDto)).willReturn(responseDto);
+
+        mockMvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(requestDto))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").exists())
+                .andExpect(jsonPath("$.username").value("soonmin"))
+                .andExpect(jsonPath("$.password").exists())
+                .andExpect(jsonPath("$.password").value("1234"))
+                .andExpect(jsonPath("$.nameCheckMessage").exists())
+                .andExpect(jsonPath("$.nameCheckMessage").value("등록 완료"))
+                .andDo(print());
+
+        verify(userService).add(requestDto);
 
     }
 }
